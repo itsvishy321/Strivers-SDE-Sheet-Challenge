c9373390a3c1bbad01200209c4a1acd9c738dddc
Vishal Verma <102803510+itsvishy321@users.noreply.github.com>
2023-06-06 19:37:13 +0530

    Create K-Max-Sum-Combinations.cpp

---
+++ Heaps/K-Max-Sum-Combinations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+vector<int> kMaxSumCombination(vector<int> &a, vector<int> &b, int n, int k){
+	vector<int> ans;
+	priority_queue<int , vector<int> , greater<int>> pq;
+
+	for(int i=0 ; i<n ; i++){
+		for(int j=0 ; j<n ; j++){
+			int sum = a[i] + b[j];
+			if(pq.size() < k){
+				pq.push(sum);
+			}
+
+			else if(sum > pq.top()){
+				pq.pop();
+				pq.push(sum);
+			}
+		}
+	}
+
+	while(!pq.empty()){
+		ans.push_back(pq.top());
+		pq.pop();
+	}
+
+	reverse(ans.begin() , ans.end());
+	return ans;
+
+}
